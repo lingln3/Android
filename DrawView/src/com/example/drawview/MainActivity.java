23b0f32f86df040ec8d542a9be5858f78f2607e4
lingln3 <lingln68@gmail.com>
2015-07-16 14:25:02 +0800

    Android 疯狂讲义

---
+++ DrawView/src/com/example/drawview/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.drawview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //获取布局文件中的linearlayout容器
+        
+    }
+
+}
